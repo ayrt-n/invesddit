4337ed9ac22866fe388742eab0b4fc979641a434
ayrton <ayrton.parkinson1@gmail.com>
2023-01-22 10:55:22 -0500

    Create Navbar component

---
+++ src/components/navbar/Navbar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Navbar() {
+  return (
+    <header className='min-h-[48px] px-[20px] bg-canvas-light border-b-1 border-nav-border'>
+      
+    </header>
+  );
+}
+
+export default Navbar;
