d384b73d8a14e377b1242cb1b295f40d591f25b8
ayrton <ayrton.parkinson1@gmail.com>
2023-01-22 10:55:41 -0500

    Add custom colors for canvas, posts, navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,14 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      'colors': {
+        'canvas': '#DAE0E6',
+        'canvas-light': '#FFFFFF',
+        'post-border': '#ccc',
+        'nav-border': '#EDEFF1',
+      },
+    },
   },
   plugins: [],
 }
