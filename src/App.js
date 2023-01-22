3266219e2b33527fe966b55585980b24a49a5354
ayrton <ayrton.parkinson1@gmail.com>
2023-01-22 15:03:04 -0500

    Add react-router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import MainFeed from './components/MainFeed';
 import Navbar from './components/navbar/Navbar';
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="bg-canvas min-h-screen">
       <Navbar />
-      <MainFeed />
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <MainFeed />
+          }
+        />
+      </Routes>
     </div>
   );
 }
