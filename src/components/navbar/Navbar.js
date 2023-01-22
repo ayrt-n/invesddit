c3624c502088094f04100de274912eb3c308ff9a
ayrton <ayrton.parkinson1@gmail.com>
2023-01-22 13:50:20 -0500

    Create empty navbar component

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Navbar() {
   return (
-    <header className='min-h-[48px] px-[20px] bg-canvas-light border-b-1 border-nav-border'>
+    <header className="min-h-[48px] px-[20px] bg-canvas-light border-b-1 border-nav-border">
       
     </header>
   );
