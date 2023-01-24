35a965f62805fec7551e74c7268137e6b8f3d279
ayrton <ayrton.parkinson1@gmail.com>
2023-01-24 12:55:54 -0500

    Add logo to navbar

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import logo from '../../assets/icons/invesddit-logo.svg'
 
 function Navbar() {
   return (
-    <header className="min-h-[48px] px-[20px] bg-canvas-light border-b-1 border-nav-border">
-      
+    <header className="min-h-[48px] px-[20px] bg-canvas-light border-b-1 border-nav-border flex items-center">
+      <img src={logo} className="w-[32px] h-[32px]" alt="invesddit logo"/>
     </header>
   );
 }
