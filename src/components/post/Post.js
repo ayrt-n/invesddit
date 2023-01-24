ed8048d775eabadce05f612338eff2d69fe8e354
ayrton <ayrton.parkinson1@gmail.com>
2023-01-24 12:55:44 -0500

    Use post props to populate data

--- src/components/post/Post.js
+++ src/components/post/Post.js
@@ -1,32 +1,16 @@
 import React from 'react';
 import PostActions from './PostActions';
+import PostMetaText from './PostMetaText';
 import PostSidebar from './PostSidebar';
 
 function Post({ post }) {
   return (
-    <div className="bg-canvas-light border-[1px] border-post-border mb-[10px] flex overflow-hidden rounded-[4px] hover:border-post-border-hover">
+    <div className="bg-canvas-light border-[1px] border-post-border mb-[10px] flex overflow-hidden rounded-[4px] hover:border-post-border-hover cursor-pointer">
       <PostSidebar />
       
       <div>
-        <div className="py-[8px] grow">
-          <div className="text-[12px] leading-[16px] mx-[8px] mb-[8px] flex">
-            <span className="font-bold hover:underline">
-              {`r/${post.community}`}
-            </span>
-            <span className="mx-[4px] text-meta-text">
-            •
-            </span>
-            <span className="mr-[3px] text-meta-text">
-              Posted by
-            </span>
-            <span className="mr-[3px] text-meta-text hover:underline">
-              u/artn
-            </span>
-            <span className="mr-[3px] text-meta-text">
-              6 hours ago
-            </span>
-          </div>
-        </div>
+        <PostMetaText community={post.community} account={post.account} createdAt={post.created_at} />
+
         <div className="px-[8px]">
           <div className="text-[18px] font-medium leading-[22px] break-all">
             {post.title}
@@ -34,11 +18,11 @@ function Post({ post }) {
         </div>
         <div className="px-[8px] mt-[5px] mb-[10px] max-h-[250px] overflow-hidden gradient-mask-b-60">
           <div className="text-[14px] leading-[21px] break-words ">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            {post.body}
           </div>
         </div>
 
-        <PostActions />
+        <PostActions comments={post.comments} />
       </div>
     </div>
   );
