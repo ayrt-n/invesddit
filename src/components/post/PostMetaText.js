2f7fba867420744476ebe14ef7fe2e995e637032
ayrton <ayrton.parkinson1@gmail.com>
2023-01-25 10:24:33 -0500

    Add delay to opening tooltips

--- src/components/post/PostMetaText.js
+++ src/components/post/PostMetaText.js
@@ -10,10 +10,31 @@ function PostMetaText({ community, account, createdAt }) {
   const [communityMenuOpen, setCommunityMenuOpen] = useState(false);
   const [accountMenuOpen, setAccountMenuOpen] = useState(false);
 
+  const [delayHandler, setDelayHandler] = useState(null)
+
+  // Event handler for showing tooltip on hover with a 500ms delay
+  const handleMouseEnter = (callback) => {
+    // Avoid resetting delay handler if already set
+    // Helps to fix bugs as mouse moves around the tooltip
+    if (delayHandler) { return }
+
+    setDelayHandler(setTimeout(() => {
+      callback(true)
+    }, 500));
+  };
+
+  // Event handler for removing the tooltip after hovering
+  // If tooltip has not been set (as a result of delay) clear the timeout
+  const handleMouseLeave = (callback) => {
+    clearTimeout(delayHandler);
+    setDelayHandler(null);
+    callback(false);
+  }
+
   return (
     <div className="py-[8px] grow">
       <div className="text-[12px] leading-[16px] mx-[8px] mb-[8px] flex">
-        <div className="relative" onMouseOver={() => setCommunityMenuOpen(true)} onMouseLeave={() => setCommunityMenuOpen(false)}>
+        <div className="relative" onMouseOver={() => handleMouseEnter(setCommunityMenuOpen)} onMouseLeave={() => handleMouseLeave(setCommunityMenuOpen)}>
           <span className="font-bold hover:underline">
             {`c/${community.sub_dir}`}
           </span>
@@ -27,7 +48,7 @@ function PostMetaText({ community, account, createdAt }) {
         <span className="mr-[3px] text-meta-text">
           Posted by
         </span>
-        <div className="relative" onMouseOver={() => setAccountMenuOpen(true)} onMouseLeave={() => setAccountMenuOpen(false)}>
+        <div className="relative" onMouseOver={() => handleMouseEnter(setAccountMenuOpen)} onMouseLeave={() => handleMouseLeave(setAccountMenuOpen)}>
           <span className="mr-[3px] text-meta-text hover:underline">
             {`u/${account.username}`}
           </span>
