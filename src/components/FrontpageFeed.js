c163b80f725f76e16c1274ad2f9f8813e21b7f35
ayrton <ayrton.parkinson1@gmail.com>
2023-01-25 08:03:19 -0500

    Call posts api to populate feed

--- src/components/FrontpageFeed.js
+++ src/components/FrontpageFeed.js
@@ -1,65 +1,15 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import FeedSidebarWelcome from './FeedSidebarWelcome';
 import Post from './post/Post';
 import FeedController from './FeedController';
+import { getPostFeed } from '../services/feedService';
 
 function MainFeed() {
-  const posts = [
-    {
-        "id": 1,
-        "title": "New post",
-        "body": "new post",
-        "created_at": "2023-01-24T20:58:46.297Z",
-        "community": {
-            "id": 1,
-            "sub_dir": "KO",
-            "description": "Community dedicated to Coca Cola ($KO)! We like the drink and love the stock!",
-            "memberships_count": 2
-        },
-        "account": {
-            "id": 1,
-            "username": "finance_dude",
-            "created_at": "2023-01-24T20:58:45.783Z"
-        },
-        "comments": []
-    },
-    {
-        "id": 2,
-        "title": "New post",
-        "body": "new post",
-        "created_at": "2023-01-24T20:58:46.301Z",
-        "community": {
-            "id": 1,
-            "sub_dir": "KO",
-            "description": "Community dedicated to Coca Cola ($KO)! We like the drink and love the stock!",
-            "memberships_count": 2
-        },
-        "account": {
-            "id": 1,
-            "username": "finance_dude",
-            "created_at": "2023-01-24T20:58:45.783Z"
-        },
-        "comments": []
-    },
-    {
-        "id": 3,
-        "title": "New post",
-        "body": "new post",
-        "created_at": "2023-01-24T20:58:46.302Z",
-        "community": {
-            "id": 2,
-            "sub_dir": "SHOP",
-            "description": "For all things Shopify! Discuss trends, investment ideas, and anything related to $SHOP!",
-            "memberships_count": 3
-        },
-        "account": {
-            "id": 1,
-            "username": "finance_dude",
-            "created_at": "2023-01-24T20:58:45.783Z"
-        },
-        "comments": []
-    }
-];
+  const [posts, setPosts] = useState([])
+
+  useEffect(() => {
+    getPostFeed().then(data => setPosts(data));
+  }, [])
 
   return (
     <div className="py-[20px] px-[24px]">
