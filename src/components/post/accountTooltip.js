0677703fdaf27e055e42a1ffade66ff2b2e2d52a
ayrton <ayrton.parkinson1@gmail.com>
2023-01-25 07:01:04 -0500

    Adjust layout of tooltip to reflect account related info

--- src/components/post/accountTooltip.js
+++ src/components/post/accountTooltip.js
@@ -1,36 +1,39 @@
 import React from 'react';
 import PillButton from '../PillButton';
 import { Link } from 'react-router-dom';
+import formatDistanceToNow from 'date-fns/formatDistanceToNow';
 import logo from '../../assets/icons/invesddit-logo.svg';
 
-function AccountTooltip({ community }) {
+function AccountTooltip({ account }) {
+  const createdAtDate = Date.parse(account.created_at);
+
   return (
     <div className="absolute top-full bg-canvas-light rounded-[4px] shadow-[0_1px_3px_0px_rgba(0,0,0,0.2)] max-w-[380px] min-w-[240px] z-50 cursor-auto">
-      <div className="p-[12px]">
-        <div className="w-full flex">
-          <img src={logo} className="rounded-full h-[32px] w-[32px] mr-[8px] align-middle shrink-0" alt="invesddit logo" />
-          <Link className="text-[16px] font-medium leading-[20px] items-center flex">
-            {`c/${community.sub_dir}`}
-          </Link>
+        <div className="p-[12px]">
+          <div className="w-full flex mb-[12px]">
+            <img src={logo} className="rounded-full h-[32px] w-[32px] mr-[8px] align-middle shrink-0" alt="invesddit logo" />
+            <div>
+              <Link className="text-[16px] font-medium leading-[20px] mr-[4px] inline-block" to={`/user/${account.username}`}>
+                {`u/${account.username}`}
+              </Link>
+              <div className="text-[12px] leading-[16px] text-meta-text">
+                <div className="inline-block">
+                  {`u/${account.username}`}
+                  <div className="mx-[3px] inline-block">
+                    •
+                  </div>
+                </div>
+                <div className="inline-block">
+                  {formatDistanceToNow(createdAtDate)} ago
+                </div>
+              </div>
+            </div>
+          </div>
+          <PillButton as={Link} to={`/user/${account.username}`}>
+            View Profile
+          </PillButton>
         </div>
-        <div className="py-[8px]">
-          <div className="text-[16px] font-medium leading-[20px]">
-            {community.memberships_count}
-          </div>
-          <div className="text-[12px] leading-[16px] text-meta-text">
-            Members
-          </div>
-        </div>
-        {community.description &&
-          <div className="text-[12px] leading-[16px] break-words py-[8px]">
-            {community.description}
-          </div>
-        }
-        <PillButton as={Link} to={`/c/${community.sub_dir}`}>
-          View Community
-        </PillButton>
       </div>
-    </div>
   );
 }
 
