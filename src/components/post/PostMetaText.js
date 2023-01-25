2d904d45f7027b3acef7e86b55639ba177b507b9
ayrton <ayrton.parkinson1@gmail.com>
2023-01-25 06:45:29 -0500

    Split out tooltip components from PostMetaText

--- src/components/post/PostMetaText.js
+++ src/components/post/PostMetaText.js
@@ -1,8 +1,7 @@
 import React, { useState } from 'react';
 import formatDistanceToNow from 'date-fns/formatDistanceToNow';
-import { Link } from 'react-router-dom';
-import PillButton from '../PillButton';
-import logo from '../../assets/icons/invesddit-logo.svg';
+import CommunityTooltip from './communityTooltip';
+import AccountTooltip from './accountTooltip';
 
 function PostMetaText({ community, account, createdAt }) {
   const createdAtDate = Date.parse(createdAt);
@@ -14,40 +13,13 @@ function PostMetaText({ community, account, createdAt }) {
   return (
     <div className="py-[8px] grow">
       <div className="text-[12px] leading-[16px] mx-[8px] mb-[8px] flex">
-        <div className="relative">
-          <div onMouseOver={() => setCommunityMenuOpen(true)} onMouseLeave={() => setCommunityMenuOpen(false)}>
-            <span className="font-bold hover:underline">
-              {`c/${community.sub_dir}`}
-            </span>
-            { communityMenuOpen &&
-              <div className="absolute top-full bg-canvas-light rounded-[4px] shadow-[0_1px_3px_0px_rgba(0,0,0,0.2)] max-w-[380px] min-w-[240px] z-50 cursor-auto">
-                <div className="p-[12px]">
-                  <div className="w-full flex">
-                    <img src={logo} className="rounded-full h-[32px] w-[32px] mr-[8px] align-middle shrink-0" alt="invesddit logo" />
-                    <Link className="text-[16px] font-medium leading-[20px] items-center flex">
-                      {`c/${community.sub_dir}`}
-                    </Link>
-                  </div>
-                  <div className="py-[8px]">
-                    <div className="text-[16px] font-medium leading-[20px]">
-                      {community.memberships_count}
-                    </div>
-                    <div className="text-[12px] leading-[16px] text-meta-text">
-                      Members
-                    </div>
-                  </div>
-                  {community.description &&
-                    <div className="text-[12px] leading-[16px] break-words py-[8px]">
-                      {community.description}
-                    </div>
-                  }
-                  <PillButton as={Link} to={`/c/${community.sub_dir}`}>
-                    View Community
-                  </PillButton>
-                </div>
-              </div>
-            }
-          </div>
+        <div className="relative" onMouseOver={() => setCommunityMenuOpen(true)} onMouseLeave={() => setCommunityMenuOpen(false)}>
+          <span className="font-bold hover:underline">
+            {`c/${community.sub_dir}`}
+          </span>
+          { communityMenuOpen &&
+            <CommunityTooltip community={community} />
+          }
         </div>
         <span className="mx-[4px] text-meta-text">
         •
@@ -55,9 +27,14 @@ function PostMetaText({ community, account, createdAt }) {
         <span className="mr-[3px] text-meta-text">
           Posted by
         </span>
-        <span className="mr-[3px] text-meta-text hover:underline">
-          {`u/${account.username}`}
-        </span>
+        <div className="relative" onMouseOver={() => setAccountMenuOpen(true)} onMouseLeave={() => setAccountMenuOpen(false)}>
+          <span className="mr-[3px] text-meta-text hover:underline">
+            {`u/${account.username}`}
+          </span>
+          { accountMenuOpen &&
+            <AccountTooltip community={community} />
+          }
+        </div>
         <span className="mr-[3px] text-meta-text">
           {formatDistanceToNow(createdAtDate)} ago
         </span>
---
+++ src/components/post/communityTooltip.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import PillButton from '../PillButton';
+import { Link } from 'react-router-dom';
+import logo from '../../assets/icons/invesddit-logo.svg';
+
+function CommunityTooltip({ community }) {
+  return (
+    <div className="absolute top-full bg-canvas-light rounded-[4px] shadow-[0_1px_3px_0px_rgba(0,0,0,0.2)] max-w-[380px] min-w-[240px] z-50 cursor-auto">
+      <div className="p-[12px]">
+        <div className="w-full flex">
+          <img src={logo} className="rounded-full h-[32px] w-[32px] mr-[8px] align-middle shrink-0" alt="invesddit logo" />
+          <Link className="text-[16px] font-medium leading-[20px] items-center flex">
+            {`c/${community.sub_dir}`}
+          </Link>
+        </div>
+        <div className="py-[8px]">
+          <div className="text-[16px] font-medium leading-[20px]">
+            {community.memberships_count}
+          </div>
+          <div className="text-[12px] leading-[16px] text-meta-text">
+            Members
+          </div>
+        </div>
+        {community.description &&
+          <div className="text-[12px] leading-[16px] break-words py-[8px]">
+            {community.description}
+          </div>
+        }
+        <PillButton as={Link} to={`/c/${community.sub_dir}`}>
+          View Community
+        </PillButton>
+      </div>
+    </div>
+  );
+}
+
+export default CommunityTooltip;
