fa7bb5eba25997b121c4877870cdcb7b64646736
ayrton <ayrton.parkinson1@gmail.com>
2023-01-25 06:23:41 -0500

    Create FeedController component

---
+++ src/components/FeedController.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function FeedController() {
+  return (
+    <div className="bg-canvas-light border-[1px] border-post-border rounded-[4px] mb-[16px] py-[10px] px-[12px] flex items-center">
+      
+    </div>
+  );
+}
+
+export default FeedController;
