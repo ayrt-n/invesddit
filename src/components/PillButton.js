ae1475a894dfe978383142137aa3a853af85522e
ayrton <ayrton.parkinson1@gmail.com>
2023-01-26 08:11:34 -0500

    Add additionalClasses prop to customize button styling

--- src/components/PillButton.js
+++ src/components/PillButton.js
@@ -5,12 +5,18 @@ const variants = {
   inverted: 'border-[1px] text-primary-500 border-primary-500 hover:bg-blue-50 active:bg-blue-100'
 }
 
-function PillButton({ as, children, variant, ...props }) {
+function PillButton({ as, children, variant, additionalClasses, ...props }) {
   const Component = as || 'button';
   const buttonStyles = variant ? variants[variant] : variants['primary'];
 
+  // Set default classes and add any additional classes specified as prop
+  let className = `${buttonStyles} font-bold py-[4px] px-[16px] rounded-full w-full cursor-pointer inline-block text-center`
+  if (additionalClasses) {
+    className = className + ' ' + additionalClasses
+  }
+
   return (
-    <Component className={`${buttonStyles} font-bold py-2 px-4 rounded-full w-full cursor-pointer inline-block text-center`} {...props}>
+    <Component className={className} {...props}>
       {children}
     </Component>
   );
