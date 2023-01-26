277fb8b49d3e252b8449229bfd3c2c6daa3d66c5
ayrton <ayrton.parkinson1@gmail.com>
2023-01-26 08:13:01 -0500

    Add upvote and downvote functions

---
+++ src/services/postService.js
@@ -0,0 +1,36 @@
+import { config } from './constants';
+
+const API_URL = config.urls.API_URL;
+
+function upvotePost(id) {
+  return fetch(`${API_URL}/api/v1/posts/${id}/votes?upvote`, {
+    method: 'POST',
+    mode: 'cors',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: 'eyJhbGciOiJIUzI1NiJ9.eyJhY2NvdW50X2lkIjoxLCJhdXRoZW50aWNhdGVkX2J5IjpbInBhc3N3b3JkIl19.1HcMMyaK69zaOBRCzwYWcZNbysFPkVR8ikacb_Hy1RA'
+    }
+  })
+  .then(response => {
+    return response.json();
+  })
+}
+
+function downvotePost(id) {
+  return fetch(`${API_URL}/api/v1/posts/${id}/votes?downvote`, {
+    method: 'POST',
+    mode: 'cors',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: 'eyJhbGciOiJIUzI1NiJ9.eyJhY2NvdW50X2lkIjoxLCJhdXRoZW50aWNhdGVkX2J5IjpbInBhc3N3b3JkIl19.1HcMMyaK69zaOBRCzwYWcZNbysFPkVR8ikacb_Hy1RA'
+    }
+  })
+  .then(response => {
+    return response.json();
+  })
+}
+
+export {
+  upvotePost,
+  downvotePost,
+}
