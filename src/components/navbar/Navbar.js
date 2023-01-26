0324e91a906d7c7473ba5913c186c05aba780af0
ayrton <ayrton.parkinson1@gmail.com>
2023-01-26 06:31:22 -0500

    Make navbar fixed

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 
 function Navbar() {
   return (
-    <header className="min-h-[48px] px-[20px] bg-canvas-light border-b-1 border-nav-border flex items-center">
+    <header className="min-h-[48px] px-[20px] bg-canvas-light border-b-[1px] border-nav-border flex items-center fixed w-full z-10">
       <Link to="/">
         <img src={logo} className="w-[32px] h-[32px]" alt="invesddit logo"/>
       </Link>
