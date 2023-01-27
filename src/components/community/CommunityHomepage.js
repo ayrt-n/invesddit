78eb68a48d638d00cd4c82188b353726d6479bd1
ayrton <ayrton.parkinson1@gmail.com>
2023-01-27 07:19:28 -0500

    Replace static data with api call to populate data

--- src/components/community/CommunityHomepage.js
+++ src/components/community/CommunityHomepage.js
@@ -4,67 +4,17 @@ import Post from '../post/Post';
 import FeedController from '../FeedController';
 import CommunityHeader from './CommunityHeader';
 import AboutCommunityWidget from './AboutCommunityWidget';
+import { getCommunity } from '../../services/communityService';
 
 function CommunityHomepage() {
   let { id } = useParams();
 
-  const community = {
-    "sub_dir": "SHOP",
-    "title": null,
-    "description": "For all things Shopify! Discuss trends, investment ideas, and anything related to $SHOP!",
-    "created_at": "2023-01-25T19:57:05.175Z",
-    "memberships_count": 3,
-    "posts": [
-        {
-            "id": 3,
-            "title": "New post",
-            "body": "new post",
-            "created_at": "2023-01-25T19:57:05.189Z",
-            "account": {
-                "id": 1,
-                "username": "finance_dude",
-                "created_at": "2023-01-25T19:57:04.671Z"
-            },
-            "comments": []
-        },
-        {
-            "id": 4,
-            "title": "New post",
-            "body": "new post",
-            "created_at": "2023-01-25T19:57:05.190Z",
-            "account": {
-                "id": 1,
-                "username": "finance_dude",
-                "created_at": "2023-01-25T19:57:04.671Z"
-            },
-            "comments": []
-        },
-        {
-            "id": 6,
-            "title": "New post",
-            "body": "new post",
-            "created_at": "2023-01-25T19:57:05.193Z",
-            "account": {
-                "id": 3,
-                "username": "bobby",
-                "created_at": "2023-01-25T19:57:05.160Z"
-            },
-            "comments": []
-        },
-        {
-            "id": 8,
-            "title": "New post",
-            "body": "new post",
-            "created_at": "2023-01-25T19:57:05.196Z",
-            "account": {
-                "id": 2,
-                "username": "ls_hedgie",
-                "created_at": "2023-01-25T19:57:04.916Z"
-            },
-            "comments": []
-        }
-    ]
-}
+  const [community, setCommunity] = useState(null);
+  useEffect(() => {
+    getCommunity(id).then(data => setCommunity(data));
+  }, [id]);
+
+  if (!community) return null;
 
   return (
     <div>
