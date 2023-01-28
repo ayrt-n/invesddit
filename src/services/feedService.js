4818c0181beba0f3ecc78be5cf7debafcbc633f2
ayrton <ayrton.parkinson1@gmail.com>
2023-01-28 09:45:48 -0500

    Add ability to filter to getPostFeed()

--- src/services/feedService.js
+++ src/services/feedService.js
@@ -2,8 +2,8 @@ import { config } from './constants';
 
 const API_URL = config.urls.API_URL;
 
-function getPostFeed() {
-  return fetch(`${API_URL}/api/v1/posts`, {
+function getPostFeed(filters = '') {
+  return fetch(`${API_URL}/api/v1/posts?${filters}`, {
     method: 'GET',
     mode: 'cors',
     headers: {
