ee56eeb13e7a6495b6612e1dd3595b9d30b0f2b8
ayrton <ayrton.parkinson1@gmail.com>
2023-01-28 12:09:51 -0500

    Create CommentSection fragment

---
+++ src/components/comments/CommentSection.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import CommentForm from './CommentForm';
+
+function CommentSection() {
+  return (
+    <>
+      <div className="my-[24px] mr-[40px] ml-[48px]">
+        <CommentForm />
+      </div>
+    </>
+  );
+}
+
+export default CommentSection;
