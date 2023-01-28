c0ce7a960b51cdff034d54027a68c705c3466f30
ayrton <ayrton.parkinson1@gmail.com>
2023-01-28 10:20:13 -0500

    Remove unused data from api call

--- src/components/community/CommunityFeed.js
+++ src/components/community/CommunityFeed.js
@@ -2,12 +2,20 @@ import React, { useState, useEffect } from 'react'
 import FeedController from '../FeedController'
 import PostPreview from '../post/PostPreview'
 import { getPostFeed } from '../../services/feedService'
+import { useParams } from 'react-router-dom'
 
-function CommunityFeed({ community }) {
+function CommunityFeed() {
+  let { id } = useParams();
+
   const [posts, setPosts] = useState(null)
   useEffect(() => {
-    getPostFeed(`community=${community}`).then(data => setPosts(data));
-  }, [community]);
+    getPostFeed(`community=${id}`).then(data => {
+      // Remove community details as not needed
+      // THIS SHOULD BE OPTIMIZED BY API NOT RENDERING THIS WHEN RETURNING COMMUNITY FEED
+      data.forEach((post) => delete post['community'])
+      setPosts(data);
+    });
+  }, [id]);
 
   if (!posts) return null;
 
