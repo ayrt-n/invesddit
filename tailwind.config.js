b322fb15158ee9f0a8730b12cca80b398f09dea0
ayrton <ayrton.parkinson1@gmail.com>
2023-01-28 12:08:31 -0500

    Add color for comment action bar

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,7 @@ module.exports = {
         'post-border-hover': '#898989',
         'nav-border': '#EDEFF1',
         'post-sidebar': '#F8F9FA',
+        'comment-controls': '#F6F7F8',
         'meta-text': '#7c7c7c',
         'icon-hover': 'rgba(26,26,27,0.1)',
         'upvote': '#cc3700',
