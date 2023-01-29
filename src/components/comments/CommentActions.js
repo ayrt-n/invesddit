0820c0f383126deba609ab17e67dd4ba869ab305
ayrton <ayrton.parkinson1@gmail.com>
2023-01-29 09:47:24 -0500

    Add toggleReply prop to comment actions

--- src/components/comments/CommentActions.js
+++ src/components/comments/CommentActions.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-function CommentActions({ score, id }) {
+function CommentActions({ score, id, toggleReply }) {
   return (
     <div className="flex items-center text-[12px] font-bold leading-[16px] my-[4px] flex-nowrap text-meta-text">
       <div className="flex items-center mr-[4px]">
@@ -18,14 +18,14 @@ function CommentActions({ score, id }) {
           </svg>
         </button>
       </div>
-      <div className="p-[8px] mr-[4px] flex items-center rounded-[2px] hover:bg-icon-hover">
+      <button onClick={toggleReply} className="p-[8px] mr-[4px] flex items-center rounded-[2px] hover:bg-icon-hover">
         <svg className="w-[24px] h-[24px] mr-[6px]" viewBox="0 0 24 24">
             <path fill="currentColor" d="M9,22A1,1 0 0,1 8,21V18H4A2,2 0 0,1 2,16V4C2,2.89 2.9,2 4,2H20A2,2 0 0,1 22,4V16A2,2 0 0,1 20,18H13.9L10.2,21.71C10,21.9 9.75,22 9.5,22V22H9M10,16V19.08L13.08,16H20V4H4V16H10Z" />
         </svg>
         <span>
           Reply
         </span>
-      </div>
+      </button>
     </div>
   );
 }
