2bc1c40878cedb5e4e6617cd23ac53febd2a791d
ayrton <ayrton.parkinson1@gmail.com>
2023-01-29 09:48:07 -0500

    Remove unneccessary margin

--- src/components/comments/CommentSidebar.js
+++ src/components/comments/CommentSidebar.js
@@ -9,7 +9,7 @@ function CommentSidebar({ account, toggleCollapse }) {
       <Link to="/user" className="my-[6px] min-h-[28px] min-w-[28px]">
         <img src={logo} alt="account avatar" className="rounded-full" />
       </Link>
-      <div onClick={toggleCollapse} className="w-full mx-5 h-full cursor-pointer bg-gradient-to-t from-canvas to-canvas bg-no-repeat bg-center hover:from-primary-500 hover:to-primary-500" style={{'background-size': '2px 100%'}} />
+      <div onClick={toggleCollapse} className="w-full h-full cursor-pointer bg-gradient-to-t from-canvas to-canvas bg-no-repeat bg-center hover:from-primary-500 hover:to-primary-500" style={{'background-size': '2px 100%'}} />
     </div>
   );
 }
