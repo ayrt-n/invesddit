da46753aa4a5e5a0f1dff7438a6e0b102dd07053
ayrton <ayrton.parkinson1@gmail.com>
2023-02-07 08:19:50 -0500

    Add buttons for feed controller

--- src/components/FeedController.js
+++ src/components/FeedController.js
@@ -1,9 +1,42 @@
 import React from 'react';
+import hotSelected from '../assets/icons/hot-selected.png';
+import hotUnselected from '../assets/icons/hot-unselected.png';
+import newSelected from '../assets/icons/new-selected.png';
+import newUnselected from '../assets/icons/new-unselected.png';
+import topSelected from '../assets/icons/top-selected.png';
+import topUnselected from '../assets/icons/top-unselected.png';
 
-function FeedController() {
+function FeedController({ sortBy, handleClick }) {
   return (
     <div className="bg-canvas-light border-[1px] border-post-border rounded-[4px] mb-[16px] py-[10px] px-[12px] flex items-center">
-      
+      <div className="items-center flex cursor-pointer">
+        {/* Sory by Hot */}
+        <button onClick={() => handleClick('hot')} className={`inline-flex py-[6px] px-[8px] rounded-[20px] font-bold leading-[17px] items-center justify-center w-auto mr-[8px] hover:bg-gray-200 focus:bg-gray-300 ${sortBy === 'hot' ? 'bg-gray-100 text-gray-100 hover:text-gray-200 focus:text-gray-300' : 'bg-inherit text-feed-text'}`}>
+          {sortBy === 'hot' ?
+            <img src={hotSelected} alt="rocketship emoji" className="w-[20px] h-[20px] mr-[8px] leading-[20px]" /> :
+            <img src={hotUnselected} alt="rocketship emoji" className="w-[20px] h-[20px] mr-[8px] leading-[20px]" />
+          }
+          <span className={`overflow-hidden text-ellipsis mr-[3px] max-h-[54px] text-left ${sortBy === 'hot' ? 'text-primary-500' : 'text-feed-text'}`}>Hot</span>
+        </button>
+
+        {/* Sort by New */}
+        <button onClick={() => handleClick('new')} className={`inline-flex py-[6px] px-[8px] rounded-[20px] font-bold leading-[17px] items-center justify-center w-auto mr-[8px] hover:bg-gray-200 focus:bg-gray-300 ${sortBy === 'new' ? 'bg-gray-100 text-gray-100 hover:text-gray-200 focus:text-gray-300' : 'bg-inherit text-feed-text'}`}>
+          {sortBy === 'new' ?
+            <img src={newSelected} alt="star emoji" className="w-[20px] h-[20px] mr-[8px] leading-[20px]" /> :
+            <img src={newUnselected} alt="star emoji" className="w-[20px] h-[20px] mr-[8px] leading-[20px]" />
+          }
+          <span className={`overflow-hidden text-ellipsis mr-[3px] max-h-[54px] text-left ${sortBy === 'new' ? 'text-primary-500' : 'text-feed-text'}`}>New</span>
+        </button>
+
+        {/* Sort by Top */}
+        <button onClick={() => handleClick('top')} className={`inline-flex py-[6px] px-[8px] rounded-[20px] font-bold leading-[17px] items-center justify-center w-auto mr-[8px] hover:bg-gray-200 focus:bg-gray-300 ${sortBy === 'top' ? 'bg-gray-100 text-gray-100 hover:text-gray-200 focus:text-gray-300' : 'bg-inherit text-feed-text'}`}>
+          {sortBy === 'top' ?
+            <img src={topSelected} alt="chart emoji" className="w-[20px] h-[20px] mr-[8px] leading-[20px]" /> :
+            <img src={topUnselected} alt="chart emoji" className="w-[20px] h-[20px] mr-[8px] leading-[20px]" />
+          }
+          <span className={`overflow-hidden text-ellipsis mr-[3px] max-h-[54px] text-left ${sortBy === 'top' ? 'text-primary-500' : 'text-feed-text'}`}>Top</span>
+        </button>
+      </div>
     </div>
   );
 }
