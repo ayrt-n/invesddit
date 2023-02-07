e09858f1620f3e1da6822119e28329a26a59bc0f
ayrton <ayrton.parkinson1@gmail.com>
2023-02-07 10:10:34 -0500

    Add useEffect and useState to populate comment data

--- src/components/comments/CommentSection.js
+++ src/components/comments/CommentSection.js
@@ -1,16 +1,26 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import CommentController from './CommentController';
 import CommentForm from './CommentForm';
 import Comment from './Comment';
+import { getComments } from '../../services/commentService';
 
-function CommentSection({ comments }) {
+function CommentSection({ postId }) {
+  const [comments, setComments] = useState([]);
+  const [sortBy, setSortBy] = useState('best');
+
+  useEffect(() => {
+    let searchParams = { sort_by: sortBy }
+
+    getComments(postId, searchParams).then(data => setComments(data.data));
+  }, [postId, sortBy]);
+
   return (
     <>
       <div className="my-[24px] mr-[40px] ml-[48px]">
         <CommentForm />
       </div>
       <div className="border-b-[1px] mx-[48px] mb-[4px] pr-[16px]">
-        <CommentController />
+        <CommentController sortBy={sortBy} setSortBy={setSortBy} />
       </div>
       <div className="pr-[16px] pb-[16px] mt-[16px] mr-[16px] ml-[10px]">
         {comments.map((comment) => {
