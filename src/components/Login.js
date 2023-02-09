80c25ed573c2582a00765a0613ff24ebd4fac8ee
ayrton <ayrton.parkinson1@gmail.com>
2023-02-09 07:10:17 -0500

    Add styling to Login component

--- src/components/Login.js
+++ src/components/Login.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Formik } from 'formik';
 import LoginInput from './forms/LoginInput';
 import { login } from '../services/authService';
+import PillButton from './PillButton';
 
 function Login() {
   // Validate email and password, return errors if present
@@ -31,21 +32,36 @@ function Login() {
   };
 
   return (
-    <div className="Form-container">
-      <h1 className="Form-header">Log in</h1>
-      <Formik
-        initialValues={{email: '', password: ''}}
-        validate={validate}
-        onSubmit={handleLogin}
-      >
-        {formik => (
-          <form onSubmit={formik.handleSubmit}>
-            <LoginInput label="Email" name="email" id="email" type="text" />
-            <LoginInput label="Password" name="password" id="password" type="password" />
-            <button type="submit">Sign in</button>
-          </form>
-        )}
-      </Formik>
+    <div className="max-w-[350px] bg-canvas-light mx-auto overflow-auto mt-4 rounded-[20px]">
+      <div className="max-w-[280px] mx-auto">
+        <h1 className="mt-[24px] font-medium text-[20px] leading-[24px]">Log in</h1>
+        <Formik
+          initialValues={{email: '', password: ''}}
+          validate={validate}
+          onSubmit={handleLogin}
+        >
+          {formik => (
+            <form onSubmit={formik.handleSubmit}>
+              <LoginInput label="Email" name="email" id="email" type="text" />
+              <LoginInput label="Password" name="password" id="password" type="password" />
+              <div className="mt-[16px]">
+                <PillButton type="submit" additionalClasses="h-[40px] leading-[18px] mt-[8px]">Log in</PillButton>
+              </div>
+            </form>
+          )}
+        </Formik>
+        <div className="text-[12px] leading-[16px] mt-[16px] mb-[24px]">
+          <div>
+            New to Invesddit?
+            <a href="#" className="underline ml-[2px] text-primary-500 font-bold leading-[24px]">Sign up</a>
+          </div>
+          <div>
+            Forgot your
+            <a href="#" className="underline ml-[2px] text-primary-500 font-bold leading-[24px]">password</a>
+            ?
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
