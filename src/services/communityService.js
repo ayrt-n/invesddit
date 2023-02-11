a28a6a0ff7c1769632a1461f2dd21aa06e12d486
ayrton <ayrton.parkinson1@gmail.com>
2023-02-11 10:05:49 -0500

    Add joinCommunity and leaveCommuntiy

--- src/services/communityService.js
+++ src/services/communityService.js
@@ -1,4 +1,5 @@
 import { config } from './constants';
+import defaultHeaders from './defaultHeaders';
 
 const API_URL = config.urls.API_URL;
 
@@ -6,15 +7,45 @@ function getCommunity(community) {
   return fetch(`${API_URL}/api/v1/communities/${community}`, {
     method: 'GET',
     mode: 'cors',
-    headers: {
-      'Content-Type': 'application/json',
-    }
+    headers: defaultHeaders(),
   })
   .then(response => {
     return response.json();
-  });
+  })
+}
+
+function joinCommunity(community) {
+  return fetch(`${API_URL}/api/v1/communities/${community}/memberships`, {
+    method: 'POST',
+    mode: 'cors',
+    headers: defaultHeaders(),
+  })
+  .then(response => {
+    if (response.ok) {
+      return response.json();
+    } else {
+      throw response.text().then(text => { throw new Error(text) });
+    }
+  })
+}
+
+function leaveCommunity(community) {
+  return fetch(`${API_URL}/api/v1/communities/${community}/memberships`, {
+    method: 'DELETE',
+    mode: 'cors',
+    headers: defaultHeaders(),
+  })
+  .then(response => {
+    if (response.ok) {
+      return response;
+    } else {
+      throw response.text().then(text => { throw new Error(text) });
+    }
+  })
 }
 
 export {
   getCommunity,
+  joinCommunity,
+  leaveCommunity,
 }
