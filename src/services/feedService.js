45a39b8bab2a1563fd10b1fa79c98878dd7426c8
ayrton <ayrton.parkinson1@gmail.com>
2023-02-11 10:06:06 -0500

    Replace headers with defaultHeaders()

--- src/services/feedService.js
+++ src/services/feedService.js
@@ -1,18 +1,15 @@
 import { config } from './constants';
-import authHeader from './authHeader';
+import defaultHeaders from './defaultHeaders';
 
 const API_URL = config.urls.API_URL;
 
 function getPostFeed(params = {}) {
-  const searchParams = new URLSearchParams(params)
+  const searchParams = new URLSearchParams(params);
 
   return fetch(`${API_URL}/api/v1/posts?${searchParams}`, {
     method: 'GET',
     mode: 'cors',
-    headers: {
-      'Content-Type': 'application/json',
-      Authorization: authHeader()
-    }
+    headers: defaultHeaders(),
   })
   .then(response => {
     return response.json();
