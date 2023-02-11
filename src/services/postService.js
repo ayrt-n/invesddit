a56bb2bdc45e93a277b7d62af9b28d151893c4b2
ayrton <ayrton.parkinson1@gmail.com>
2023-02-11 12:26:35 -0500

    Remove upvotePost and downvotePost in favour of independent voteServer

--- src/services/postService.js
+++ src/services/postService.js
@@ -15,36 +15,6 @@ function getPost(id) {
   })
 }
 
-function upvotePost(id) {
-  return fetch(`${API_URL}/api/v1/posts/${id}/votes?upvote`, {
-    method: 'POST',
-    mode: 'cors',
-    headers: {
-      'Content-Type': 'application/json',
-      Authorization: 'eyJhbGciOiJIUzI1NiJ9.eyJhY2NvdW50X2lkIjoxLCJhdXRoZW50aWNhdGVkX2J5IjpbInBhc3N3b3JkIl19.1HcMMyaK69zaOBRCzwYWcZNbysFPkVR8ikacb_Hy1RA'
-    }
-  })
-  .then(response => {
-    return response.json();
-  })
-}
-
-function downvotePost(id) {
-  return fetch(`${API_URL}/api/v1/posts/${id}/votes?downvote`, {
-    method: 'POST',
-    mode: 'cors',
-    headers: {
-      'Content-Type': 'application/json',
-      Authorization: 'eyJhbGciOiJIUzI1NiJ9.eyJhY2NvdW50X2lkIjoxLCJhdXRoZW50aWNhdGVkX2J5IjpbInBhc3N3b3JkIl19.1HcMMyaK69zaOBRCzwYWcZNbysFPkVR8ikacb_Hy1RA'
-    }
-  })
-  .then(response => {
-    return response.json();
-  })
-}
-
 export {
   getPost,
-  upvotePost,
-  downvotePost,
 }
