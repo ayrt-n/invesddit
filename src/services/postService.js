9528f327b8cc93c109da2262be2cdc8c5d3dc51f
ayrton <ayrton.parkinson1@gmail.com>
2023-02-12 09:01:11 -0500

    Use defaultHeaders in api calls

--- src/services/postService.js
+++ src/services/postService.js
@@ -1,4 +1,5 @@
 import { config } from './constants';
+import defaultHeaders from './defaultHeaders';
 
 const API_URL = config.urls.API_URL;
 
@@ -6,9 +7,7 @@ function getPost(id) {
   return fetch(`${API_URL}/api/v1/posts/${id}`, {
     method: 'GET',
     mode: 'cors',
-    headers: {
-      'Content-Type': 'application/json'
-    }
+    headers: defaultHeaders(),
   })
   .then(response => {
     return response.json();
