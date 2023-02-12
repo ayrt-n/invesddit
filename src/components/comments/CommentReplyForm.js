611283ecb3c396fe9bead1c3bbce8b9772f14ce9
ayrton <ayrton.parkinson1@gmail.com>
2023-02-12 09:02:05 -0500

    Change class to className

--- src/components/comments/CommentReplyForm.js
+++ src/components/comments/CommentReplyForm.js
@@ -4,7 +4,7 @@ import CommentForm from './CommentForm';
 function CommentReplyForm() {
   return (
     <div className="flex w-full">
-      <div class="my-[16px] ml-[22px] p-[2px] grow">
+      <div className="my-[16px] ml-[22px] p-[2px] grow">
         <CommentForm autoFocus />
       </div>
     </div>
