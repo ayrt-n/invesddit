fded361638b387b8664ba661adddf099c2c7b597
ayrton <ayrton.parkinson1@gmail.com>
2023-02-14 07:14:17 -0500

    Refactor to use useFormik hook and accept/set values based on external events

--- src/components/TextPostForm.js
+++ src/components/TextPostForm.js
@@ -1,9 +1,9 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import TextInput from './forms/TextInput';
 import PillButton from './PillButton';
-import { Formik } from 'formik';
-import TextareaInput from './forms/TextareaInput';
+import { useFormik } from 'formik';
 import HiddenInput from './forms/HiddenInput';
+import TextareaInput from './forms/TextareaInput';
 import { createPost } from '../services/postService';
 import { useNavigate } from 'react-router-dom';
 
@@ -12,7 +12,7 @@ function TextPostForm({ community }) {
 
   const validate = (values) => {
     const errors = {};
-
+    
     if (!values.title) { errors.title = 'Required' }
     if (!values.community) { errors.title = 'Required' }
 
@@ -26,47 +26,62 @@ function TextPostForm({ community }) {
     .catch((err) => {
       console.error(err);
     });
-  }
+  };
+
+  const formik = useFormik({
+    initialValues: {
+      title: '',
+      body: '',
+      community: '',
+    },
+    validate: validate,
+    onSubmit: handlePost,
+  });
+
+  useEffect(() => {
+    if (community) {
+      formik.setFieldValue('community', community);
+    }
+  }, [community]);
 
   return (
-    <>
-      <Formik
-        initialValues={{
-          title: '',
-          body: '',
-          community: community,
-        }}
-        validate={validate}
-        onSubmit={handlePost}
-      >
-        {formik => (
-          <form onSubmit={formik.handleSubmit}>
-            <HiddenInput id="community" name="community" />
-            <div className="m-[16px]">
-              <div className="mb-[8px]">
-                <div className="relative mb-[6px]">
-                  <TextInput type="text" placeholder="Title" showLength maxLength={300} id="title" name="title" />
-                </div>
-                <div>
-                  <TextareaInput placeholder="Text (optional)" id="body" name="body" />
-                </div>
-              </div>
+    <form onSubmit={formik.handleSubmit}>
+      <HiddenInput {...formik.getFieldProps('community') } />
+      <div className="m-[16px]">
+        <div className="mb-[8px]">
+          <div className="relative mb-[6px]">
+            <TextInput
+              type="text"
+              placeholder="Title"
+              showLength
+              maxLength={300}
+              id="title"
+              name="title"
+              {...formik.getFieldProps('title')}
+            />
+          </div>
+          <div>
+            <TextareaInput
+              placeholder="Text (optional)"
+              id="body"
+              name="body"
+              {...formik.getFieldProps('body')}  
+            />
+          </div>
+        </div>
+      </div>
+      <div className="px-[16px] pb-[16px]">
+        <div className="w-full">
+          <div className="flex items-center flex-row-reverse justify-between">
+            <div>
+              <PillButton type="Submit" disabled={!formik.isValid || !formik.dirty} >
+                Post
+              </PillButton>
             </div>
-            <div className="px-[16px] pb-[16px]">
-              <div className="w-full">
-                <div className="flex items-center flex-row-reverse justify-between">
-                  <div>
-                    <PillButton type="Submit" disabled={!formik.isValid || !formik.dirty} >
-                      Post
-                    </PillButton>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </form>
-        )}
-      </Formik>
-    </>
+          </div>
+        </div>
+      </div>
+    </form>
   );
 }
 
