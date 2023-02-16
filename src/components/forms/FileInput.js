8d271feb1e5fb9f1b9386c164807d8848b468446
ayrton <ayrton.parkinson1@gmail.com>
2023-02-16 06:30:56 -0500

    Fix bug with clearing and reuploading same image

--- src/components/forms/FileInput.js
+++ src/components/forms/FileInput.js
@@ -1,8 +1,8 @@
-import React, { useRef, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import PillButton from '../PillButton';
 
-function FileInput({ setMedia, ...props }) {
-  const [imagePreview, setImagePreview] = useState(null);
+function FileInput({ onChange, value, ...props }) {
+  const [imagePreview, setImagePreview] = useState('');
   const uploadRef = useRef(null);
 
   const uploadFile = () => {
@@ -11,28 +11,31 @@ function FileInput({ setMedia, ...props }) {
 
   const handleChange = (event) => {
     if (event.target.files && event.target.files[0]) {
-      setMedia(event.target.value);
-      setImagePreview(URL.createObjectURL(event.target.files[0]));
+      onChange(event.target.value);
     }
   };
 
-  const clear = () => {
-    setMedia('');
-    setImagePreview(null);
-  };
+  // Set image preview based on value of uploaded file or empty
+  useEffect(() => {
+    if (uploadRef.current.files && uploadRef.current.files[0]) {
+      setImagePreview(URL.createObjectURL(uploadRef.current.files[0]))
+    } else {
+      setImagePreview('');
+    }
+  }, [value]);
 
   return (
     <div className="rounded-[4px]">
-      <input ref={uploadRef} onChange={handleChange} {...props} type="file" accept="image/png,img/gif,image/jpeg,image/webp" className="hidden" />
+      <input value={value} ref={uploadRef} onChange={handleChange} {...props} type="file" accept="image/png,img/gif,image/jpeg,image/webp" className="hidden" />
       {imagePreview ?
         <div className="p-[12px] flex justify-center flex-col nowrap min-h-[280px] text-center border-[1px] border-nav-border rounded-[4px]">
           <img onClick={uploadFile} src={imagePreview} className="rounded-[4px] w-auto cursor-pointer" alt="preview of upload" />
-          <button type="button" onClick={clear}>Clear</button>
+          <button type="button" onClick={() => onChange('')}>Clear</button>
         </div> :
         <div className="flex justify-center flex-col nowrap min-h-[280px] text-center border-dashed border-[1px] border-nav-border rounded-[4px]">
           <p className="text-primary-500">
             Drag and drop image or
-            <PillButton onClick={uploadFile} variant="inverted" additionalClasses="inline-block max-w-[min-content] mx-[8px] my-[10px]">
+            <PillButton type="button" onClick={uploadFile} variant="inverted" additionalClasses="inline-block max-w-[min-content] mx-[8px] my-[10px]">
               Upload
             </PillButton>
           </p>
