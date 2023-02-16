7d61de3ccc0ca308f990026171868a90774387fa
ayrton <ayrton.parkinson1@gmail.com>
2023-02-16 06:42:11 -0500

    Add close button for clearing image

--- src/components/forms/FileInput.js
+++ src/components/forms/FileInput.js
@@ -28,9 +28,14 @@ function FileInput({ onChange, value, ...props }) {
     <div className="rounded-[4px]">
       <input value={value} ref={uploadRef} onChange={handleChange} {...props} type="file" accept="image/png,img/gif,image/jpeg,image/webp" className="hidden" />
       {imagePreview ?
-        <div className="p-[12px] flex justify-center flex-col nowrap min-h-[280px] text-center border-[1px] border-nav-border rounded-[4px]">
+        <div className="group p-[12px] relative flex justify-center flex-col nowrap min-h-[280px] text-center border-[1px] border-nav-border rounded-[4px]">
           <img onClick={uploadFile} src={imagePreview} className="rounded-[4px] w-auto cursor-pointer" alt="preview of upload" />
-          <button type="button" onClick={() => onChange('')}>Clear</button>
+          <button className="hidden group-hover:block absolute top-[6px] right-[6px] rounded-full bg-canvas-light border-none p-0" type="button" onClick={() => onChange('')}>
+            <svg className="w-[25px] h-[25px]" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+              <title>close-circle</title>
+              <path d="M12,2C17.53,2 22,6.47 22,12C22,17.53 17.53,22 12,22C6.47,22 2,17.53 2,12C2,6.47 6.47,2 12,2M15.59,7L12,10.59L8.41,7L7,8.41L10.59,12L7,15.59L8.41,17L12,13.41L15.59,17L17,15.59L13.41,12L17,8.41L15.59,7Z" />
+            </svg>
+          </button>
         </div> :
         <div className="flex justify-center flex-col nowrap min-h-[280px] text-center border-dashed border-[1px] border-nav-border rounded-[4px]">
           <p className="text-primary-500">
