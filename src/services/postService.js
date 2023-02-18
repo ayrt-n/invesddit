b973942a10befbbac73e581d276102ea5f1955d4
ayrton <ayrton.parkinson1@gmail.com>
2023-02-18 09:27:57 -0500

    Create functions to create media, link, and text posts

--- src/services/postService.js
+++ src/services/postService.js
@@ -1,5 +1,6 @@
 import { config } from './constants';
 import defaultHeaders from './defaultHeaders';
+import authHeader from './authHeader';
 
 const API_URL = config.urls.API_URL;
 
@@ -15,13 +16,63 @@ function getPost(id) {
 }
 
 function createPost(values) {
-  return fetch(`${API_URL}/api/v1/communities/${values.community}/posts`, {
+  switch(values.postType) {
+    case 'media':
+      return createMediaPost(values);
+    case 'link':
+      return createLinkPost(values);
+    default:
+      return createTextPost(values);
+  }
+}
+
+function createTextPost(values) {
+  return fetch(`${API_URL}/api/v1/communities/${values.community}/text_posts`, {
     method: 'POST',
     mode: 'cors',
     headers: defaultHeaders(),
     body: JSON.stringify({
       'title': values.title,
       'body': values.body
+    })
+  })
+  .then(response => {
+    if (response.ok) {
+      return response.json();
+    } else {
+      throw response.text().then(text => { throw new Error(text) });
+    }
+  });
+}
+
+function createMediaPost(values) {
+  const formData = new FormData;
+  formData.append("post[title]", values.title)
+  formData.append("post[image]", values.media)
+
+  return fetch(`${API_URL}/api/v1/communities/${values.community}/media_posts`, {
+    method: 'POST',
+    mode: 'cors',
+    headers: { 'Authorization': authHeader() },
+    body: formData
+  })
+  .then(response => {
+    if (response.ok) {
+      return response.json();
+    } else {
+      throw response.text().then(text => { throw new Error(text) });
+    }
+  });
+}
+
+function createLinkPost(values) {
+  return fetch(`${API_URL}/api/v1/communities/${values.community}/link_posts`, {
+    method: 'POST',
+    mode: 'cors',
+    headers: defaultHeaders(),
+    body: JSON.stringify({
+      'title': values.title,
+      'body': values.link,
     })
   })
   .then(response => {
