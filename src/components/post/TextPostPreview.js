a3543203d52efa5c18299368135b35de5f557a1e
ayrton <ayrton.parkinson1@gmail.com>
2023-02-18 10:50:44 -0500

    Create TextPost components

---
+++ src/components/post/TextPostPreview.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function TextPostPreview({ body }) {
+  return (
+    <div className="px-[8px] mt-[5px] mb-[10px] max-h-[250px] overflow-hidden gradient-mask-b-60">
+      <div className="text-[14px] leading-[21px] break-all">
+        {body}
+      </div>
+    </div>
+  );
+}
+
+export default TextPostPreview;
