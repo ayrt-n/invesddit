8e5461ef60aaf5a9e468368ceb5728bd098e92ce
ayrton <ayrton.parkinson1@gmail.com>
2023-02-18 09:27:30 -0500

    Remove value from input and pass file on change

--- src/components/forms/FileInput.js
+++ src/components/forms/FileInput.js
@@ -11,14 +11,14 @@ function FileInput({ onChange, value, ...props }) {
 
   const handleChange = (event) => {
     if (event.target.files && event.target.files[0]) {
-      onChange(event.target.value);
+      onChange(event.target.files[0]);
     }
   };
 
   // Set image preview based on value of uploaded file or empty
   useEffect(() => {
-    if (uploadRef.current.files && uploadRef.current.files[0]) {
-      setImagePreview(URL.createObjectURL(uploadRef.current.files[0]))
+    if (value) {
+      setImagePreview(URL.createObjectURL(value))
     } else {
       setImagePreview('');
     }
@@ -26,7 +26,7 @@ function FileInput({ onChange, value, ...props }) {
 
   return (
     <div className="rounded-[4px]">
-      <input value={value} ref={uploadRef} onChange={handleChange} {...props} type="file" accept="image/png,img/gif,image/jpeg,image/webp" className="hidden" />
+      <input ref={uploadRef} onChange={handleChange} {...props} type="file" accept="image/png,img/gif,image/jpeg,image/webp" className="hidden" />
       {imagePreview ?
         <div className="group p-[12px] relative flex justify-center flex-col nowrap min-h-[280px] text-center border-[1px] border-nav-border rounded-[4px]">
           <img onClick={uploadFile} src={imagePreview} className="rounded-[4px] w-auto cursor-pointer" alt="preview of upload" />
