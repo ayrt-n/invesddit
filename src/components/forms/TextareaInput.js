c26f071c845c263ca99feb851131b0b82895be48
ayrton <ayrton.parkinson1@gmail.com>
2023-02-23 06:51:56 -0500

    Add ability to show remaining length

--- src/components/forms/TextareaInput.js
+++ src/components/forms/TextareaInput.js
@@ -1,8 +1,22 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
-function TextareaInput({ maxLength, ...props }) {
+function TextareaInput({ showLength, ...props }) {
+  // State to track the length of the text input
+  // Will be displayed with the input if showLength prop true
+  const [length, setLength] = useState(props.value.length);
+  useEffect(() => { setLength(props.value.length) }, [props.value]);
+
+
   return (
-    <textarea {...props} className="outline-0 break-words rounded-[4px] border-[1px] border-nav-border w-full min-h-[122px] overflow-hidden py-[8px] px-[16px] resize-vertical text-[14px] leading-[21px] active:border-input-focused focus:border-input-focused" />
+    <>
+      <textarea {...props} className="outline-0 break-words rounded-[4px] border-[1px] border-nav-border w-full min-h-[122px] overflow-hidden py-[8px] px-[16px] resize-vertical text-[14px] leading-[21px] active:border-input-focused focus:border-input-focused align-top" />
+      {showLength ?
+        <div className="text-feed-text pt-[5px] text-[12px] leading-[16px]">
+          {props.maxLength - length} characters remaining
+        </div> :
+        null
+      }
+    </>
   )
 }
 
