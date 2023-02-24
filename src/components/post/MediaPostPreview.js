dfea0dbaeb32ae136c9aaa49cde22048a1b47652
ayrton <ayrton.parkinson1@gmail.com>
2023-02-24 07:38:02 -0500

    Add some margin below image

--- src/components/post/MediaPostPreview.js
+++ src/components/post/MediaPostPreview.js
@@ -5,7 +5,7 @@ function MediaPostPreview({ media }) {
     <div className="mt-[8px] w-full max-h-[512px] mx-auto flex justify-center">
       <div className="text-[14px] leading-[21px] break-all">
         <div>
-          <img className="max-h-[512px] mx-auto max-w-full" src={media} alt="user post" />
+          <img className="max-h-[512px] mx-auto max-w-full mb-[4px]" src={media} alt="user post" />
         </div>
       </div>
     </div>
