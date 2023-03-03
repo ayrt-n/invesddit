763cc2464f18de68de128d099f8c1a8829ff5ec2
ayrton <ayrton.parkinson1@gmail.com>
2023-03-03 09:46:21 -0500

    Update to use account avatar if present

--- src/components/post/accountTooltip.js
+++ src/components/post/accountTooltip.js
@@ -2,7 +2,7 @@ import React from 'react';
 import PillButton from '../PillButton';
 import { Link } from 'react-router-dom';
 import formatDistanceToNow from 'date-fns/formatDistanceToNow';
-import logo from '../../assets/icons/invesddit-logo.svg';
+import defaultAvatar from '../../assets/icons/invesddit-logo.svg';
 
 function AccountTooltip({ account }) {
   const createdAtDate = Date.parse(account.created_at);
@@ -15,7 +15,7 @@ function AccountTooltip({ account }) {
     <div onClick={stopWrappedLinks} className="absolute top-full bg-canvas-light rounded-[4px] shadow-[0_1px_3px_0px_rgba(0,0,0,0.2)] max-w-[380px] min-w-[240px] z-50 cursor-auto">
         <div className="p-[12px]">
           <div className="w-full flex mb-[12px]">
-            <img src={logo} className="rounded-full h-[32px] w-[32px] mr-[8px] align-middle shrink-0" alt="invesddit logo" />
+            <img src={account.avatar || defaultAvatar} className="rounded-full h-[32px] w-[32px] mr-[8px] align-middle shrink-0" alt="invesddit logo" />
             <div>
               <Link className="text-[16px] font-medium leading-[20px] mr-[4px] inline-block" to={`/user/${account.username}`}>
                 {`u/${account.username}`}
