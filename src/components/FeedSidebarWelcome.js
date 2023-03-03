ecfb147527bd35b12e76f03029bc9cb5e11d8440
ayrton <ayrton.parkinson1@gmail.com>
2023-03-03 08:08:46 -0500

    Add banner image across widget

--- src/components/FeedSidebarWelcome.js
+++ src/components/FeedSidebarWelcome.js
@@ -4,23 +4,26 @@ import { Link } from 'react-router-dom';
 
 function FeedSidebarWelcome() {
   return (
-    <div className="bg-canvas-light p-[12px] rounded-[4px] border-[1px] border-post-border">
-      <div className="text-[16px] font-semibold leading-[20px] mb-[8px]">
-        Home
-      </div>
-      <div className="mb-[8px] text-[14px] leading-[21px] break-words">
-        Your personal Invesddit frontpage. Come here to check in with your favorite communities
-      </div>
-      <hr className="h-[1px] my-[16px]" />
-      <div>
-        <PillButton as={Link} to="/submit" variant="primary">
-          Create Post
-        </PillButton>
-      </div>
-      <div className="mt-[12px]">
-        <PillButton as={Link} to="/communities/new" variant="inverted">
-          Create Community
-        </PillButton>
+    <div className="rounded-[4px] border-[1px] border-post-border overflow-hidden">
+      <div className="bg-stock-banner h-[34px] bg-cover bg-position-center bg-no-repeat" />
+      <div className="bg-canvas-light p-[12px]">
+        <div className="text-[16px] font-semibold leading-[20px] mb-[8px]">
+          Home
+        </div>
+        <div className="mb-[8px] text-[14px] leading-[21px] break-words">
+          Your personal Invesddit frontpage. Come here to check in with your favorite communities
+        </div>
+        <hr className="h-[1px] my-[16px]" />
+        <div>
+          <PillButton as={Link} to="/submit" variant="primary">
+            Create Post
+          </PillButton>
+        </div>
+        <div className="mt-[12px]">
+          <PillButton as={Link} to="/communities/new" variant="inverted">
+            Create Community
+          </PillButton>
+        </div>
       </div>
     </div>
   );
