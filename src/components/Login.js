bf91e07ed3582107c10208b8aee4a02ae5444065
ayrton <ayrton.parkinson1@gmail.com>
2023-03-06 07:53:38 -0500

    Add error message to Login component

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,23 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Formik } from 'formik';
 import LoginInput from './forms/LoginInput';
 import { login } from '../services/authService';
 import LoginButton from './forms/LoginButton';
 import { useNavigate } from 'react-router-dom';
+import ErrorMessage from './forms/ErrorMessage';
 
 function Login() {
   const navigate = useNavigate();
+  const [errorMessage, setErrorMessage] = useState(null);
 
   // Validate email and password, return errors if present
   const validate = (values) => {
     const errors = {};
 
     if (!values.email) {
-      errors.email = 'Required';
+      errors.email = 'Please enter an email address to continue';
+    } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+      errors.email = 'Not a valid email address';
     }
 
     if (!values.password) {
-      errors.password = 'Required';
+      errors.password = 'Please enter a password to continue';
     }
 
     return errors;
@@ -27,19 +31,25 @@ function Login() {
   // If success, redirect user to app
   // Otherwise, display error messages
   const handleLogin = (values, { setSubmitting }) => {
+    setErrorMessage('');
+
     login(values.email, values.password)
     .then((data) => {
       console.log(data);
-      setSubmitting(false);
-      navigate('/');
-      window.location.reload();
+      if (data.success) {
+        navigate('/');
+        window.location.reload();
+      } else {
+        setErrorMessage(data['field-error'][1]);
+        setSubmitting(false);
+      }
     })
   };
 
   return (
-    <div className="max-w-[350px] bg-canvas-light mx-auto overflow-auto mt-4 rounded-[20px]">
+    <div className="max-w-[400px] bg-canvas-light mx-auto overflow-auto mt-4 rounded-[20px]">
       <div className="max-w-[280px] mx-auto">
-        <h1 className="mt-[24px] font-medium text-[20px] leading-[24px]">Log in</h1>
+        <h1 className="mt-[24px] font-medium text-[20px] leading-[24px] mb-[24px]">Log in</h1>
         <Formik
           initialValues={{email: '', password: ''}}
           validate={validate}
@@ -49,18 +59,21 @@ function Login() {
             <form onSubmit={formik.handleSubmit}>
               <LoginInput label="Email" name="email" id="email" type="text" />
               <LoginInput label="Password" name="password" id="password" type="password" />
-              <LoginButton />
+              {errorMessage && <ErrorMessage>{errorMessage}</ErrorMessage>}
+              <LoginButton>
+                Log in
+              </LoginButton>
             </form>
           )}
         </Formik>
         <div className="text-[12px] leading-[16px] mt-[16px] mb-[24px]">
           <div>
             New to Invesddit?
-            <a href="#" className="underline ml-[2px] text-primary-500 font-bold leading-[24px]">Sign up</a>
+            <a href="/signup" className="underline ml-[2px] text-primary-500 font-bold leading-[24px]">Sign up</a>
           </div>
           <div>
             Forgot your
-            <a href="#" className="underline ml-[2px] text-primary-500 font-bold leading-[24px]">password</a>
+            <a href="/forgot-password" className="underline ml-[2px] text-primary-500 font-bold leading-[24px]">password</a>
             ?
           </div>
         </div>
