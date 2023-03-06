b789f2f0bd7fabbbabec54d93be2c750b9a7b199
ayrton <ayrton.parkinson1@gmail.com>
2023-03-06 07:52:34 -0500

    Replace login button with PillButton component

--- src/components/forms/LoginButton.js
+++ src/components/forms/LoginButton.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import PillButton from '../PillButton';
 
-function LoginButton() {
+function LoginButton({ children }) {
   return (
     <div className="mt-[16px]">
-      <PillButton type="submit" additionalClasses="h-[40px] leading-[18px] mt-[8px]">Log in</PillButton>
+      <PillButton type="submit" additionalClasses="h-[40px] leading-[18px] mt-[8px]">
+        {children}
+      </PillButton>
   </div>
   );
 }
