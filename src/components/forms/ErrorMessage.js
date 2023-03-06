e747b31ba9a1235a4dfcf6fe85787866afbadcda
ayrton <ayrton.parkinson1@gmail.com>
2023-03-06 07:51:52 -0500

    Add padding to container div

--- src/components/forms/ErrorMessage.js
+++ src/components/forms/ErrorMessage.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
-function ErrorMessage({ children }) {
+function ErrorMessage({ styles, children }) {
   return (
-    <div className="text-[12px] leading-[16px] text-[red] pt-[4px]">
-      <span className="nowrap overflow-hidden text-ellipsis">
+    <div className="text-[12px] leading-[16px] text-[red] pt-[4px] pl-[16px]">
+      <span className="nowrap overflow-hidden text-ellipsis normal-case" style={styles}>
         {children}
       </span>
     </div>
