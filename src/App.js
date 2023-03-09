18dbae03c66c73504c861667c8b2e99092dc3a1f
ayrton <ayrton.parkinson1@gmail.com>
2023-03-09 12:53:01 -0500

    Add ModalProvider to App

--- src/App.js
+++ src/App.js
@@ -13,30 +13,38 @@ import Profile from './components/profile/Profile';
 import AccountProvider from './contexts/account/AccountProvider';
 import CreateCommunityPage from './components/community/CreateCommunityPage';
 import SignUp from './components/SignUp';
+import VerifyAccount from './components/VerifyAccount';
+import ModalProvider from './contexts/modal/ModalProvider';
 
 function App() {
   return (
     <AccountProvider>
-      <div className="bg-canvas min-h-screen">
-        <Navbar />
-        <div className="pt-[48px]">
-          <Routes>
-            <Route index element={<Homepage />} />
-            <Route path="submit" element={<CreatePostPage />} />
-            <Route path="login" element={<Login />} />
-            <Route path="signup" element={<SignUp />} />
-            <Route path="communities/new" element={<CreateCommunityPage />} />
-            <Route path="c/:community_id" element={<CommunityDashboard />}>
-              <Route index element={<CommunityFeed />} />
-              <Route path="posts/:post_id" element={<Post />} />
-            </Route>
-            <Route path="c/:community_id/settings" element={<CommunitySettings />} />
-            <Route path="profile/settings" element={<ProfileSettings/>} />
-            <Route path="profile/:username" element={<Profile />} />
-          </Routes>
+      <ModalProvider>
+        <div className="bg-canvas min-h-screen">
+          <div>
+            
+          </div>
+          <Navbar />
+          <div className="pt-[48px]">
+            <Routes>
+              <Route index element={<Homepage />} />
+              <Route path="submit" element={<CreatePostPage />} />
+              <Route path="login" element={<Login />} />
+              <Route path="signup" element={<SignUp />} />
+              <Route path="verify-account" element={<VerifyAccount />} />
+              <Route path="communities/new" element={<CreateCommunityPage />} />
+              <Route path="c/:community_id" element={<CommunityDashboard />}>
+                <Route index element={<CommunityFeed />} />
+                <Route path="posts/:post_id" element={<Post />} />
+              </Route>
+              <Route path="c/:community_id/settings" element={<CommunitySettings />} />
+              <Route path="profile/settings" element={<ProfileSettings/>} />
+              <Route path="profile/:username" element={<Profile />} />
+            </Routes>
+          </div>
         </div>
-      </div>
-  </AccountProvider>
+      </ModalProvider>
+    </AccountProvider>
   );
 }
 
