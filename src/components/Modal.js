4dc9fa034718382a60567b49b973e65d4377d8d8
ayrton <ayrton.parkinson1@gmail.com>
2023-03-16 06:53:19 -0400

    Add aria attributes for accessibility

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -2,6 +2,17 @@ import React, { useEffect } from 'react';
 import ReactDOM from 'react-dom';
 
 function Modal({ isOpen, content, closeModal }) {
+  // Disable keyboard interactions with all non-modal elements on mount
+  useEffect(() => {
+    const rootDiv = document.getElementById('root');
+    rootDiv.ariaHidden = "true"
+
+    return(() => {
+      const rootDiv = document.getElementById('root');
+      rootDiv.ariaHidden = "false"
+    })
+  }, [])
+
   useEffect(() => {
     // Add listener to close modal if esc key pressed
     const escapeKey = (e) => {
@@ -24,7 +35,7 @@ function Modal({ isOpen, content, closeModal }) {
   }, [isOpen, closeModal])
 
   if (isOpen) return ReactDOM.createPortal(
-    <div className="fixed top-0 left-0 h-screen w-full z-[50]" style={{ background: "rgba(0,0,0,0.4)" }}>
+    <div role="dialog" aria-modal="true" className="fixed top-0 left-0 h-screen w-full z-[50]" style={{ background: "rgba(0,0,0,0.4)" }}>
       <div className="max-h-[90%] max-w-[400px] w-[90%] fixed top-[50%] left-[50%] -translate-y-[50%] -translate-x-[50%] overflow-hidden">
         {content}
       </div>
