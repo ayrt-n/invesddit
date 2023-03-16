4104b3d45205e7b4a1f8ed830a3a194721d6b289
ayrton <ayrton.parkinson1@gmail.com>
2023-03-16 07:12:55 -0400

    Add conditional render of notification icon

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -48,17 +48,19 @@ function Navbar() {
         </div>
       </div>
       <div className="flex items-center ml-[8px] pl-[8px] border-l-[1px] border-nav-border">
-        <button className="mr-[8px] h-[32px] w-[32px] relative rounded-[4px] hover:bg-nav-icon-hov active:bg-nav-icon-active">
-          <svg className="h-[20px] w-[20px] text-nav-icon align-middle leading-[20px] m-auto" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-            <title>bell-outline</title>
-            <path fill="currentColor" d="M10 21H14C14 22.1 13.1 23 12 23S10 22.1 10 21M21 19V20H3V19L5 17V11C5 7.9 7 5.2 10 4.3V4C10 2.9 10.9 2 12 2S14 2.9 14 4V4.3C17 5.2 19 7.9 19 11V17L21 19M17 11C17 8.2 14.8 6 12 6S7 8.2 7 11V18H17V11Z" />
-          </svg>
-        </button>
         {isLoggedIn() ?
-          <div ref={dropdown} onClick={toggleDropdown}>
-            <NavDropdown />
-            {dropdownOpen ? <NavDropdownMenu /> : null}
-          </div> :
+          <>
+            <button className="mr-[8px] h-[32px] w-[32px] relative rounded-[4px] hover:bg-nav-icon-hov active:bg-nav-icon-active">
+              <svg className="h-[20px] w-[20px] text-nav-icon align-middle leading-[20px] m-auto" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                <title>bell-outline</title>
+                <path fill="currentColor" d="M10 21H14C14 22.1 13.1 23 12 23S10 22.1 10 21M21 19V20H3V19L5 17V11C5 7.9 7 5.2 10 4.3V4C10 2.9 10.9 2 12 2S14 2.9 14 4V4.3C17 5.2 19 7.9 19 11V17L21 19M17 11C17 8.2 14.8 6 12 6S7 8.2 7 11V18H17V11Z" />
+              </svg>
+            </button>
+            <div ref={dropdown} onClick={toggleDropdown}>
+              <NavDropdown />
+              {dropdownOpen ? <NavDropdownMenu /> : null}
+            </div>
+          </> :
           <>
             <PillButton onClick={() => openModal(<OnboardModal initialState="signup"/>)} variant="inverted" additionalClasses="ml-[4px] text-[14px] whitespace-nowrap">
               Sign up
