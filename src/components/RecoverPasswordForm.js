da8463aeac5e0eb58966e88aad04b2f114ab3550
ayrton <ayrton.parkinson1@gmail.com>
2023-03-27 06:49:24 -0400

    Add reset password request form

---
+++ src/components/RecoverPasswordForm.js
@@ -0,0 +1,68 @@
+import React, { useState } from 'react';
+import { Formik } from 'formik';
+import LoginInput from './forms/LoginInput';
+import LoginButton from './forms/LoginButton';
+import ErrorMessage from './forms/ErrorMessage';
+import { resetPasswordRequest } from '../services/authService';
+
+function RecoverPasswordForm({ links }) {
+  const [errorMessage, setErrorMessage] = useState(null);
+  const [submit, setSubmit] = useState(false);
+
+  const validate = (values) => {
+    const errors = {}
+
+    if (!values.email) {
+      errors.email = 'Please enter an email address to continue';
+    } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+      errors.email = 'Not a valid email address';
+    }
+
+    return errors;
+  };
+
+  const handleSubmit = (values, { setSubmitting }) => {
+    resetPasswordRequest(values.email, values.username, values.password, values.passwordConfirmation)
+    .then((data) => {
+      if (data.success) {
+        setSubmit('success');
+      } else {
+        setErrorMessage(() => {
+          if (data['field-error']) return data['field-error'][1];
+          
+          return data['error'];
+        })
+        setSubmitting(false);
+      }
+    })
+  };
+
+  return (
+    <div className="max-w-[280px] mx-auto">
+      <div className="my-[24px]">
+        <h1 className="font-medium text-[20px] leading-[24px]">Reset your password</h1>
+        <p className="mt-[8px] text-[12px] leading-[16px]">
+          Tell us the email address associated with your Invesddit account, and we'll send you an email with a link to reset your password.
+        </p>
+      </div>
+      <Formik
+        initialValues={{email: ''}}
+        validate={validate}
+        onSubmit={handleSubmit}
+      >
+        {({ isValid, dirty, isSubmitting, handleSubmit }) => (
+          <form onSubmit={handleSubmit}>
+            <LoginInput label="Email" name="email" id="email" type="text" />
+            {errorMessage && <ErrorMessage>{errorMessage}</ErrorMessage>}
+            <LoginButton disabled={!isValid || !dirty || isSubmitting}>
+              Reset password
+            </LoginButton>
+          </form>
+        )}
+      </Formik>
+      {links}
+    </div>
+  );
+}
+
+export default RecoverPasswordForm;
