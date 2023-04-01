d2e8ccefb6ecdd2471fe29ca1c742e0af12008db
ayrton <ayrton.parkinson1@gmail.com>
2023-04-01 08:51:56 -0400

    Set width to full

--- src/components/AuthLayout.js
+++ src/components/AuthLayout.js
@@ -4,7 +4,7 @@ import banner from '../assets/images/auth-banner.png'
 
 function AuthLayout() {
   return (
-    <div className="bg-canvas-light min-h-screen font-sans flex">
+    <div className="bg-canvas-light min-h-screen font-sans flex w-full">
       <div className="w-[156px] bg-no-repeat bg-cover" style={{backgroundImage: `url(${banner})`}} />
       <div className="p-[24px]">
         <Outlet />
