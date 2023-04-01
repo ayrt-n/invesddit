0922419c49ecb2dc7e4142b7ca6568d932d18a74
ayrton <ayrton.parkinson1@gmail.com>
2023-04-01 08:51:13 -0400

    Add pasword recovery sent success message

--- src/components/RecoverPasswordForm.js
+++ src/components/RecoverPasswordForm.js
@@ -4,6 +4,7 @@ import LoginInput from './forms/LoginInput';
 import LoginButton from './forms/LoginButton';
 import ErrorMessage from './forms/ErrorMessage';
 import { resetPasswordRequest } from '../services/authService';
+import SuccessMessage from './SuccessMessage';
 
 function RecoverPasswordForm({ links }) {
   const [errorMessage, setErrorMessage] = useState(null);
@@ -28,10 +29,14 @@ function RecoverPasswordForm({ links }) {
         setSubmit('success');
       } else {
         setErrorMessage(() => {
-          if (data['field-error']) return data['field-error'][1];
+          // Return error message with first letter capitalized
+          if (data['field-error']) {
+            return data['field-error'][1].charAt(0).toUpperCase() + data['field-error'][1].slice(1);
+          }
           
           return data['error'];
         })
+        
         setSubmitting(false);
       }
     })
@@ -39,28 +44,35 @@ function RecoverPasswordForm({ links }) {
 
   return (
     <div className="max-w-[280px] mx-auto">
-      <div className="my-[24px]">
-        <h1 className="font-medium text-[20px] leading-[24px]">Reset your password</h1>
-        <p className="mt-[8px] text-[12px] leading-[16px]">
-          Tell us the email address associated with your Invesddit account, and we'll send you an email with a link to reset your password.
-        </p>
-      </div>
-      <Formik
-        initialValues={{email: ''}}
-        validate={validate}
-        onSubmit={handleSubmit}
-      >
-        {({ isValid, dirty, isSubmitting, handleSubmit }) => (
-          <form onSubmit={handleSubmit}>
-            <LoginInput label="Email" name="email" id="email" type="text" />
-            {errorMessage && <ErrorMessage>{errorMessage}</ErrorMessage>}
-            <LoginButton disabled={!isValid || !dirty || isSubmitting}>
-              Reset password
-            </LoginButton>
-          </form>
-        )}
-      </Formik>
-      {links}
+      {submit ?
+        <div className="my-[24px]">
+          <SuccessMessage header="Password Reset Sent" message="Check your email for a link to reset your password." />
+        </div> :
+        <>
+          <div className="my-[24px]">
+            <h1 className="font-medium text-[20px] leading-[24px]">Reset your password</h1>
+            <p className="mt-[8px] text-[12px] leading-[16px]">
+              Tell us the email address associated with your Invesddit account, and we'll send you an email with a link to reset your password.
+            </p>
+          </div>
+          <Formik
+            initialValues={{email: ''}}
+            validate={validate}
+            onSubmit={handleSubmit}
+          >
+            {({ isValid, dirty, isSubmitting, handleSubmit }) => (
+              <form onSubmit={handleSubmit}>
+                <LoginInput label="Email" name="email" id="email" type="text" />
+                {errorMessage && <ErrorMessage>{errorMessage}</ErrorMessage>}
+                <LoginButton disabled={!isValid || !dirty || isSubmitting}>
+                  Reset password
+                </LoginButton>
+              </form>
+            )}
+          </Formik>
+          {links}
+        </>
+      }
     </div>
   );
 }
