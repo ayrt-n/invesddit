1b1a640574c8299e4b6e34ffd88108a934715a55
ayrton <ayrton.parkinson1@gmail.com>
2023-04-03 06:41:58 -0400

    Create userAvatar and communityAvatar components

--- src/components/Avatar.js
+++ src/components/Avatar.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import defaultUserAvatar from '../assets/icons/invesddit-logo.svg';
+import defaultCommunityAvatar from '../assets/icons/invesddit-logo.svg';
 
-function Avatar({ classNames, src, alt }) {
+export default function Avatar({ classNames, src, alt }) {
   return (
     <img className={`rounded-full object-cover align-middle overflow-hidden ${classNames}`} src={src} alt={alt} />
   );
 }
 
-export default Avatar;
+export function UserAvatar({ classNames, src, alt, ...props }) {
+  return (
+    <Avatar classNames={classNames} src={src || defaultUserAvatar} alt={alt} {...props} />
+  );
+}
+
+export function CommunityAvatar({ classNames, src, alt, ...props }) {
+  return (
+    <Avatar classNames={classNames} src={src || defaultCommunityAvatar} alt={alt} {...props} />
+  );
+}
