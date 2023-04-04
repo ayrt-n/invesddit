a106b05f3cccae181190d4a0b31daa7384e439a0
ayrton <ayrton.parkinson1@gmail.com>
2023-04-04 05:48:35 -0400

    Replace div with h1

--- src/components/DangerMessage.js
+++ src/components/DangerMessage.js
@@ -10,9 +10,9 @@ function DangerMessage({ header, message, children }) {
             <path fill="currentColor" d="M11,15H13V17H11V15M11,7H13V13H11V7M12,2C6.47,2 2,6.5 2,12A10,10 0 0,0 12,22A10,10 0 0,0 22,12A10,10 0 0,0 12,2M12,20A8,8 0 0,1 4,12A8,8 0 0,1 12,4A8,8 0 0,1 20,12A8,8 0 0,1 12,20Z" />
           </svg>
         </div>
-        <div className="text-[24px] font-medium">
+        <h1 className="text-[24px] font-medium">
           {header}
-        </div>
+        </h1>
       </div>
       <div className="text-[18px] text-center mb-[16px]">
         {message}
