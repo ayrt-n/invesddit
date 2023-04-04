d6e33dda6673bdcef2222bd401ac88b96eaade9d
ayrton <ayrton.parkinson1@gmail.com>
2023-04-04 07:03:45 -0400

    Remove userEvent import

--- src/components/__tests__/FeedController.test.js
+++ src/components/__tests__/FeedController.test.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { screen, render } from '@testing-library/react';
 import '@testing-library/jest-dom';
-import userEvent from '@testing-library/user-event';
 import FeedController from '../FeedController';
 import { MemoryRouter } from 'react-router-dom';
 
