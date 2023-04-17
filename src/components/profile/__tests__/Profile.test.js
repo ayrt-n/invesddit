149dca8a4b11c2a112c3b18f1d3a289e7588fc4c
ayrton <ayrton.parkinson1@gmail.com>
2023-04-17 07:08:53 -0400

    Add test for profile

---
+++ src/components/profile/__tests__/Profile.test.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { screen, render } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import Profile from '../Profile';
+
+// Mocks
+jest.mock('react-router-dom', () => ({
+  ...jest.requireActual('react-router-dom'),
+  useParams: () => ({
+    username: 'test_user',
+  }),
+}));
+
+jest.mock('../ProfileWidget', () => ({ username }) => (
+  <div data-testid='widget'>
+    {username}
+  </div>
+));
+
+jest.mock('../../Feed', () => ({ subdir }) => (
+    <div data-testid="feed">
+      {subdir}
+    </div>
+));
+
+jest.mock('../../FeedController', () => () => (
+  <div />
+))
+
+// Profile Test
+describe('Profile component', () => {
+  describe('renders component', () => {
+    it('correctly renders component for user based on search params', () => {
+      render(<Profile />)
+
+      expect(screen.getByTestId('widget')).toHaveTextContent('test_user');
+      expect(screen.getByTestId('feed')).toHaveTextContent('api/v1/accounts/test_user/posts');
+    });
+  });
+});
