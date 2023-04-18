7e0e7acff1e7f5b1a10582f42b77e06442605cc4
ayrton <ayrton.parkinson1@gmail.com>
2023-04-18 08:04:26 -0400

    Refactor to use HoverableTooltip component

--- src/components/comments/CommentMetaText.js
+++ src/components/comments/CommentMetaText.js
@@ -1,34 +1,11 @@
-import React, { useState } from 'react';
+import React from 'react';
 import formatDistanceToNow from 'date-fns/formatDistanceToNow';
 import AccountTooltip from '../post/AccountTooltip';
 import { Link } from 'react-router-dom';
+import HoverableTooltip from '../tooltips/HoverableTooltip';
 
 function CommentMetaText({ account, createdAt, status }) {
   const createdAtDate = Date.parse(createdAt);
-
-  // States to determine whether pop-up menus should be open/closed
-  const [accountMenuOpen, setAccountMenuOpen] = useState(false);
-
-  const [delayHandler, setDelayHandler] = useState(null)
-
-  // Event handler for showing tooltip on hover with a 500ms delay
-  const handleMouseEnter = (callback) => {
-    // Avoid resetting delay handler if already set
-    // Helps to fix bugs as mouse moves around the tooltip
-    if (delayHandler) { return }
-
-    setDelayHandler(setTimeout(() => {
-      callback(true)
-    }, 500));
-  };
-
-  // Event handler for removing the tooltip after hovering
-  // If tooltip has not been set (as a result of delay) clear the timeout
-  const handleMouseLeave = (callback) => {
-    clearTimeout(delayHandler);
-    setDelayHandler(null);
-    callback(false);
-  }
 
   // If status is deleted, render mock up of Comment Meta Text
   if (status === 'deleted') {
@@ -56,16 +33,13 @@ function CommentMetaText({ account, createdAt, status }) {
   return (
     <div className="mt-[10px] mb-[6px] min-h-[18px]">
       <div className="text-[12px] leading-[16px] flex">
-        <div className="relative" onMouseOver={() => handleMouseEnter(setAccountMenuOpen)} onMouseLeave={() => handleMouseLeave(setAccountMenuOpen)}>
+        <HoverableTooltip tooltipComponent={<AccountTooltip account={account} />}>
           <Link to={`/profile/${account.username}`}>
             <span className="font-medium hover:underline">
               {`${account.username}`}
             </span>
           </Link>
-          { accountMenuOpen &&
-            <AccountTooltip account={account} />
-          }
-        </div>
+        </HoverableTooltip>
         <span className="mx-[4px] text-meta-text">
           •
         </span>
