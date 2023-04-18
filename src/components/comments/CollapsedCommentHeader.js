3ad25c213ebf3fc739410ad84f8394f3cea5df9c
ayrton <ayrton.parkinson1@gmail.com>
2023-04-18 06:41:42 -0400

    Change margin to match rest of comment components

--- src/components/comments/CollapsedCommentHeader.js
+++ src/components/comments/CollapsedCommentHeader.js
@@ -4,7 +4,7 @@ import CommentMetaText from './CommentMetaText';
 function CollapsedCommentHeader({ comment, toggleCollapse }) {
   return (
     <div className="pt-[8px] pl-[8px] flex items-center">
-      <button onClick={toggleCollapse} className="my-[6px] mr-[8px]">
+      <button onClick={toggleCollapse} className="mt-[10px] mb-[6px] mr-[8px]">
         <svg className="w-[16px] h-[16px] text-primary-500" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
           <title>arrow-expand</title>
           <path fill="currentColor" d="M10,21V19H6.41L10.91,14.5L9.5,13.09L5,17.59V14H3V21H10M14.5,10.91L19,6.41V10H21V3H14V5H17.59L13.09,9.5L14.5,10.91Z" />
