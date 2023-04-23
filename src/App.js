623db89b0654468c13b68231439bd2a2733e9c11
ayrton <ayrton.parkinson1@gmail.com>
2023-04-23 17:18:55 -0400

    Add AuthProvider to App

--- src/App.js
+++ src/App.js
@@ -21,101 +21,104 @@ import AuthLayout from './components/AuthLayout';
 import RecoverPassword from './components/RecoverPassword';
 import PublicRoute from './components/common/PublicRoute';
 import PrivateRoute from './components/common/PrivateRoute';
+import AuthProvider from './contexts/authentication/AuthProvider';
 
 function App() {
   return (
-    <AccountProvider>
-      <ModalProvider>
-        <Routes>
-          {/* Regular Routes - Includes both public and private routes */}
-          <Route path="/" element={<Layout />}>
-            <Route index element={<Homepage />} />
-            <Route path="c/:community_id" element={<CommunityDashboard />}>
-              <Route index element={<CommunityFeed />} />
-              <Route path="posts/:post_id" element={<Post />} />
+    <AuthProvider>
+      <AccountProvider>
+        <ModalProvider>
+          <Routes>
+            {/* Regular Routes - Includes both public and private routes */}
+            <Route path="/" element={<Layout />}>
+              <Route index element={<Homepage />} />
+              <Route path="c/:community_id" element={<CommunityDashboard />}>
+                <Route index element={<CommunityFeed />} />
+                <Route path="posts/:post_id" element={<Post />} />
+              </Route>
+              <Route path="profile/:username" element={<Profile />} />
+              <Route path="search" element={<SearchResults />} />
+              <Route
+                path="submit"
+                element={
+                  <PrivateRoute>
+                    <CreatePostPage />
+                  </PrivateRoute>
+                }
+              />
+              <Route
+                path="communities/new"
+                element={
+                  <PrivateRoute>
+                    <CreateCommunityPage />
+                  </PrivateRoute>
+                }
+              />
+              <Route
+                path="c/:community_id/settings"
+                element={
+                  <PrivateRoute>
+                    <CommunitySettings />
+                  </PrivateRoute>
+                }
+              />
+              <Route
+                path="profile/settings"
+                element={
+                  <PrivateRoute>
+                    <ProfileSettings/>
+                  </PrivateRoute>
+                }
+              />
+              <Route
+                path="notifications"
+                element={
+                  <PrivateRoute>
+                    <NotificationDashboard />
+                  </PrivateRoute>
+                }
+              />
             </Route>
-            <Route path="profile/:username" element={<Profile />} />
-            <Route path="search" element={<SearchResults />} />
-            <Route
-              path="submit"
-              element={
-                <PrivateRoute>
-                  <CreatePostPage />
-                </PrivateRoute>
-              }
-            />
-            <Route
-              path="communities/new"
-              element={
-                <PrivateRoute>
-                  <CreateCommunityPage />
-                </PrivateRoute>
-              }
-            />
-            <Route
-              path="c/:community_id/settings"
-              element={
-                <PrivateRoute>
-                  <CommunitySettings />
-                </PrivateRoute>
-              }
-            />
-            <Route
-              path="profile/settings"
-              element={
-                <PrivateRoute>
-                  <ProfileSettings/>
-                </PrivateRoute>
-              }
-            />
-            <Route
-              path="notifications"
-              element={
-                <PrivateRoute>
-                  <NotificationDashboard />
-                </PrivateRoute>
-              }
-            />
-          </Route>
 
-          {/* Routes related to authentication which require different layout */}
-          <Route path="/" element={<AuthLayout />}>
-            <Route
-              path="login"
-              element={
-                <PublicRoute>
-                  <Login />
-                </PublicRoute>
-              }
-            />
-            <Route
-              path="signup"
-              element={
-                <PublicRoute>
-                  <SignUp />
-                </PublicRoute>
-              }
-            />
-            <Route
-              path="forgot-password"
-              element={
-                <PublicRoute>
-                  <RecoverPassword />
-                </PublicRoute>
-              }
-            />
-            <Route
-              path="verify-account"
-              element={
-                <PublicRoute>
-                  <VerifyAccount />
-                </PublicRoute>
-              }
-            />
-          </Route>
-        </Routes>
-      </ModalProvider>
-    </AccountProvider>
+            {/* Routes related to authentication which require different layout */}
+            <Route path="/" element={<AuthLayout />}>
+              <Route
+                path="login"
+                element={
+                  <PublicRoute>
+                    <Login />
+                  </PublicRoute>
+                }
+              />
+              <Route
+                path="signup"
+                element={
+                  <PublicRoute>
+                    <SignUp />
+                  </PublicRoute>
+                }
+              />
+              <Route
+                path="forgot-password"
+                element={
+                  <PublicRoute>
+                    <RecoverPassword />
+                  </PublicRoute>
+                }
+              />
+              <Route
+                path="verify-account"
+                element={
+                  <PublicRoute>
+                    <VerifyAccount />
+                  </PublicRoute>
+                }
+              />
+            </Route>
+          </Routes>
+        </ModalProvider>
+      </AccountProvider>
+    </AuthProvider>
   );
 }
 
