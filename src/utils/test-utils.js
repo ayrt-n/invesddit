deb59912b9ee80b60931a0be3e3cff2f6984319e
ayrton <ayrton.parkinson1@gmail.com>
2023-04-25 07:30:22 -0400

    Create testing utilities with custom render

---
+++ src/utils/test-utils.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import AuthContext from '../contexts/authentication/AuthContext';
+import AccountContext from '../contexts/account/AccountContext';
+import ModalContext from '../contexts/modal/ModalContext';
+
+// Custom render method to wrap all renders in React context
+// Easily set context values for authenticated and account via authContext and accountContext args
+function customRender(childComponent, {authValues, accountValues, modalValues, ...options} = {}) {
+  function ProviderWrapper({children}) {
+    return (
+      <AuthContext.Provider value={authValues || {}}>
+        <AccountContext.Provider value={accountValues || {}}>
+          <ModalContext.Provider value={modalValues || {}}>
+            {children}
+          </ModalContext.Provider>
+        </AccountContext.Provider>
+      </AuthContext.Provider>
+    );
+  }
+
+  return render(childComponent, {wrapper: ProviderWrapper, ...options});
+}
+
+export * from '@testing-library/react'
+export {customRender as render}
