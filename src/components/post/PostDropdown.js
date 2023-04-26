c3c1595714760799e9c9a2c47d2905cdc48ed165
ayrton <ayrton.parkinson1@gmail.com>
2023-04-26 18:05:19 -0400

    Add aria label

--- src/components/post/PostDropdown.js
+++ src/components/post/PostDropdown.js
@@ -5,7 +5,7 @@ import DropdownItem from '../dropdown/DropdownItem';
 import ConfirmationButton from '../ConfirmationButton';
 
 function PostDropdown({ deletePost, editPost }) {
-  const dropdownButton = <svg className="w-[24px] h-[24px]" viewBox="0 0 24 24">
+  const dropdownButton = <svg className="w-[24px] h-[24px]" viewBox="0 0 24 24" aria-label="post options">
                            <path fill="currentColor" d="M16,12A2,2 0 0,1 18,10A2,2 0 0,1 20,12A2,2 0 0,1 18,14A2,2 0 0,1 16,12M10,12A2,2 0 0,1 12,10A2,2 0 0,1 14,12A2,2 0 0,1 12,14A2,2 0 0,1 10,12M4,12A2,2 0 0,1 6,10A2,2 0 0,1 8,12A2,2 0 0,1 6,14A2,2 0 0,1 4,12Z" />
                          </svg>
 
