e0637cbe3d73b174ad54beafa5eaeb2430222d0c
ayrton <ayrton.parkinson1@gmail.com>
2023-04-27 08:02:21 -0400

    Import mock post data

--- src/components/post/__tests__/PostContent.test.js
+++ src/components/post/__tests__/PostContent.test.js
@@ -3,28 +3,11 @@ import '@testing-library/jest-dom';
 import { render, screen } from '../../../utils/test-utils';
 import PostContent from '../PostContent';
 import { MemoryRouter } from 'react-router-dom';
+import { textPost, linkPost, mediaPost, deletedPost } from '../../../mocks/data/posts';
 
 describe('Post Content component', () => {
   describe('when post status media', () => {
     it('renders media post content', () => {
-      const mediaPost = {
-        status: 'media',
-        id: 1,
-        type: "MediaPost",
-        title: 'Test Post',
-        comments_count: 0,
-        created_at: "2023-04-14T13:49:33.617Z",
-        score: 1,
-        content: "test.jpg",
-        vote_status: null,
-        account: {
-            id: 15,
-            username: "Test_user",
-            avatar: null,
-            created_at: "2023-04-14T13:47:48.760Z"
-        }
-      };
-
       render(
         <MemoryRouter>
           <PostContent post={mediaPost} deletePost={jest.fn()} editPost={jest.fn()} />
@@ -40,24 +23,6 @@ describe('Post Content component', () => {
 
   describe('when post status link', () => {
     it('renders link post content', () => {
-      const linkPost = {
-        status: 'link',
-        id: 1,
-        type: "LinkPost",
-        title: 'Test Post',
-        comments_count: 0,
-        created_at: "2023-04-14T13:49:33.617Z",
-        score: 1,
-        content: "https://test.com",
-        vote_status: null,
-        account: {
-            id: 15,
-            username: "Test_user",
-            avatar: null,
-            created_at: "2023-04-14T13:47:48.760Z"
-        }
-      };
-
       render(
         <MemoryRouter>
           <PostContent post={linkPost} deletePost={jest.fn()} editPost={jest.fn()} />
@@ -73,24 +38,6 @@ describe('Post Content component', () => {
 
   describe('when post status text', () => {
     it('renders text post content', () => {
-      const textPost = {
-        status: 'text',
-        id: 1,
-        type: "TextPost",
-        title: 'Test Post',
-        comments_count: 0,
-        created_at: "2023-04-14T13:49:33.617Z",
-        score: 1,
-        content: "Test text post, say that three times fast",
-        vote_status: null,
-        account: {
-            id: 15,
-            username: "Test_user",
-            avatar: null,
-            created_at: "2023-04-14T13:47:48.760Z"
-        }
-      };
-
       render(
         <MemoryRouter>
           <PostContent post={textPost} deletePost={jest.fn()} editPost={jest.fn()} />
@@ -106,19 +53,6 @@ describe('Post Content component', () => {
 
   describe('when post status deleted', () => {
     it('renders text post content', () => {
-      const deletedPost = {
-        status: 'deleted',
-        id: 1,
-        type: "TextPost",
-        title: 'Test Post',
-        comments_count: 0,
-        created_at: "2023-04-14T13:49:33.617Z",
-        score: 1,
-        content: "[Deleted]",
-        vote_status: null,
-        account: null
-      };
-
       render(
         <MemoryRouter>
           <PostContent post={deletedPost} deletePost={jest.fn()} editPost={jest.fn()} />
