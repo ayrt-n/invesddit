22fd46384f6f333e5a5a2a71669f04e2d78fc255
ayrton <ayrton.parkinson1@gmail.com>
2023-05-02 07:33:02 -0400

    Add mock onChange

--- src/components/forms/__tests__/TextInput.test.js
+++ src/components/forms/__tests__/TextInput.test.js
@@ -6,7 +6,7 @@ import TextInput from '../TextInput';
 describe('Text Input component', () => {
   describe('with show length enabled', () => {
     it('renders the length of the value prop supplied', () => {
-      render(<TextInput showLength value="123456" maxLength="300" />)
+      render(<TextInput showLength value="123456" onChange={jest.fn()} maxLength="300" />)
       
       expect(screen.getByTestId('showLength')).toHaveTextContent("6/300")
     });
@@ -14,7 +14,7 @@ describe('Text Input component', () => {
 
   describe('with show length disabled', () => {
     it('does not render show length component', () => {
-      render(<TextInput value="123456" maxLength="300" />)
+      render(<TextInput value="123456" onChange={jest.fn()} maxLength="300" />)
       
       expect(screen.queryByTestId('showLength')).not.toBeInTheDocument();
     });
--- src/components/forms/__tests__/TextareaInput.test.js
+++ src/components/forms/__tests__/TextareaInput.test.js
@@ -6,13 +6,13 @@ import TextareaInput from '../TextareaInput';
 describe('Text Input component', () => {
   describe('with show length enabled', () => {
     it('renders the remaining characters based on value/max length', () => {
-      render(<TextareaInput showLength value="123456" maxLength="30" />)
+      render(<TextareaInput showLength value="123456" onChange={jest.fn()} maxLength="30" />)
       
       expect(screen.getByTestId('remainingCharacters')).toHaveTextContent(/24 characters remaining/i)
     });
 
     it('renders the remaining characters if value is blank', () => {
-      render(<TextareaInput showLength value="" maxLength="30" />)
+      render(<TextareaInput showLength value="" onChange={jest.fn()} maxLength="30" />)
       
       expect(screen.getByTestId('remainingCharacters')).toHaveTextContent(/30 characters remaining/i)
     });
@@ -20,7 +20,7 @@ describe('Text Input component', () => {
 
   describe('with show length disabled', () => {
     it('does not render show length component', () => {
-      render(<TextareaInput value="123456" maxLength="30" />)
+      render(<TextareaInput value="123456" onChange={jest.fn()} maxLength="30" />)
       
       expect(screen.queryByTestId('remainingCharacters')).not.toBeInTheDocument();
     });
