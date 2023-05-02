c13b016a8b60afeb18cf4b19b395416d28290e2f
ayrton <ayrton.parkinson1@gmail.com>
2023-05-02 07:01:08 -0400

    Add data-testid

--- src/components/forms/TextareaInput.js
+++ src/components/forms/TextareaInput.js
@@ -6,12 +6,11 @@ function TextareaInput({ showLength, ...props }) {
   const [length, setLength] = useState(props.value.length);
   useEffect(() => { setLength(props.value.length) }, [props.value]);
 
-
   return (
     <>
       <textarea {...props} className="outline-0 break-words rounded-[4px] border-[1px] border-nav-border w-full min-h-[122px] overflow-hidden py-[8px] px-[16px] resize-vertical text-[14px] leading-[21px] active:border-input-focused focus:border-input-focused align-top" />
       {showLength ?
-        <div className="text-feed-text pt-[5px] text-[12px] leading-[16px]">
+        <div className="text-feed-text pt-[5px] text-[12px] leading-[16px]" data-testid="remainingCharacters">
           {props.maxLength - length} characters remaining
         </div> :
         null
